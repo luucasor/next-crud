6d6fb4ea8c46d1d89b9a7ae6139fa4a9b02ae47c
luucasor <lucas@jornadatecnologia.com>
2023-07-11 14:34:57 -0300

    Adicionada novo componente para tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente("Ana", 34, '1'),
+    new Cliente("Bia", 45, '1'),
+    new Cliente("Carlos", 23, '1'),
+    new Cliente("Pedro", 54, '1'),
+  ]
   return (
     <div className={`
       flex h-screen justify-center items-center
@@ -8,7 +17,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteúdo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
